884b3254ac3c6cda5343fecf60667cd0e9b86734
Amir Hariri <a.r.hariri2000@gmail.com>
2026-01-06 11:56:48 -0400

    multi sensor

--- libemg/_datasets/emg_epn100.py
+++ libemg/_datasets/emg_epn100.py
@@ -6,7 +6,7 @@ from libemg.feature_extractor import FeatureExtractor
 from libemg.utils import *
 
 class EMGEPN100(Dataset):
-    def __init__(self, dataset_file='DATASET_85', cross_user=True):
+    def __init__(self, dataset_file: str = 'DATASET_85', cross_user: bool = True):
         split = '30 Reps x 12 Gestures x 43 Users (Train), 15 Reps x 12 Gestures x 42 Users (Test) --> Cross User Split'
         if not cross_user:
             split = '15 Reps x 12 Gestures (Train), 15 Reps x 12 Gestures (Test) from the 43 Test Users --> User Dependent Split'
@@ -14,7 +14,7 @@ class EMGEPN100(Dataset):
         Dataset.__init__(self, 
                          500, 
                          8, 
-                         'gForce', 
+                         ('Myo', 'gForce'), 
                          85, 
                          {  "relax": 0,       # Matches EPN-612 static classes IDs
                             "fist": 1,
@@ -31,6 +31,7 @@ class EMGEPN100(Dataset):
                          split,
                          "EMG dataset for 12 different hand gesture categories using the Myo armband and the G-force armband.", 
                          'https://doi.org/10.3390/s22249613')
+        self.resolution_bit = (8, 12)
         self.url = "https://nextcloud.epn.edu.ec/index.php/s/wmPWpFWSo4XxLLA/download"
         self.dataset_name = dataset_file
 
